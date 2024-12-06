9d880a6467e1c4d390407e6ea9a04795bd570237
42reddy <110433102+42reddy@users.noreply.github.com>
2024-12-06 22:51:59 +0100

    Update underdamped.py

--- underdamped.py
+++ underdamped.py
@@ -1,15 +1,17 @@
 from KineticsSandbox.system import system
 from KineticsSandbox.integrator import D1_stochastic
 import numpy as np
+from scipy import constants
+
+kb = constants.R * 0.001
 
 
 class underdamped():
 
-    def __init__(self, system, n_states, lagtime, tau, V_simulation, V_target):
+    def __init__(self, system, n_states, tau, V_simulation, V_target):
 
         self.system = system
         self.n_states = n_states
-        self.lagtime = lagtime
         self.tau = tau
         self.V_simulation = V_simulation
         self.V_target = V_target
@@ -42,11 +44,10 @@ class underdamped():
         """
 
         D1_stochastic.B_step(self.system, potential, half_step=True)
+        delta_eta = (np.exp(-self.system.xi * self.system.dt) * (self.system.dt / 2) * (self.gradient(self.system.x)[0])
+                     / (np.sqrt(kb * self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt)))))
         D1_stochastic.A_step(self.system, half_step=True)
-        D1_stochastic.O_step(self.system, eta_k=eta_k)
-        delta_eta = np.exp(-self.system.xi * self.system.dt) * (self.system.dt / 2) * (self.gradient(self.system.x))[
-            0] / (
-                        np.sqrt(self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt))))
+        D1_stochastic.O_step(self.system, eta_k=eta_k[0])
         D1_stochastic.A_step(self.system, half_step=True)
         D1_stochastic.B_step(self.system, potential, half_step=True)
 
@@ -72,16 +73,16 @@ class underdamped():
         D1_stochastic.B_step(self.system, potential, half_step=True)
         D1_stochastic.O_step(self.system, half_step=True, eta_k=eta_k[0])
         delta_eta = np.exp(-self.system.xi * self.system.dt / 2) * (self.system.dt / 2) * (self.gradient(self.system.x))[
-            0] / (np.sqrt(self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt / 2))))
+            0] / (np.sqrt(kb * self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt / 2))))
         D1_stochastic.A_step(self.system)
         D1_stochastic.O_step(self.system, half_step=True, eta_k=eta_k[1])
         delta_eta1 = (self.system.dt / 2) * (self.gradient(self.system.x))[
-            0] / (np.sqrt(self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt))))
+            0] / (np.sqrt(kb * self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt))))
         D1_stochastic.B_step(self.system, potential, half_step=True)
 
         return delta_eta, delta_eta1
 
-    def OABAO(self, potential, eta_k=None):
+    def ABOBA(self, potential, eta_k=None):
         """
         Perform a full Langevin integration step for the OABAO algorithm
 
@@ -98,18 +99,15 @@ class underdamped():
         None: The function modifies the 'x' and 'v' attributes of the provided system object in place.
         """
 
-        D1_stochastic.O_step(self.system, half_step=True, eta_k=eta_k[0])
-        delta_eta = (self.system.dt / 2) * (self.gradient(self.system.x))[0] / (
-            np.sqrt(self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt / 2))))
-        D1_stochastic.A_step(self.system)
         D1_stochastic.A_step(self.system, half_step=True)
         D1_stochastic.B_step(self.system, potential, half_step=True)
+        D1_stochastic.O_step(self.system, eta_k=eta_k[0])
+        delta_eta = (np.exp(- self.system.xi * self.system.dt) + 1) * (self.gradient(self.system.x)[0] * self.system.dt / 2) / (
+            np.sqrt(kb * self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt))))
+        D1_stochastic.B_step(self.system, potential, half_step=True)
         D1_stochastic.A_step(self.system, half_step=True)
-        D1_stochastic.O_step(self.system, half_step=True, eta_k=eta_k[1])
-        delta_eta1 = np.exp(-self.system.xi * self.system.dt / 2) * (self.system.dt / 2) *(self.gradient(self.system.x))[
-                        0] / (np.sqrt(self.system.T * self.system.m * (1 - np.exp(-2 * self.system.xi * self.system.dt / 2))))
 
-        return delta_eta, delta_eta1
+        return delta_eta
 
     def generate(self, N):
 
@@ -117,31 +115,33 @@ class underdamped():
         simulates the system at double well potential and Euler Maruyama scheme
         """
         delta_eta = np.zeros(N)
-        #delta_eta1 = np.zeros(N)
+        delta_eta1 = np.zeros(N)
         eta1 = np.zeros(N)
-        #eta2 = np.zeros(N)
+        eta2 = np.zeros(N)
         X = np.zeros(N)
         v = np.zeros(N)
         X[0] = self.system.x
         for i in range(N - 1):
             eta1[i] = np.random.normal(0, 1)  # random number corresponding to random force
-            #eta2[i] = np.random.normal(0,1)
-            delta_eta[i] = self.BAOAB(self.V_simulation, eta_k=eta1[i])
+            eta2[i] = np.random.normal(0, 1)
+            eta = [eta1[i], eta2[i]]
+            delta_eta[i] = self.ABOBA(self.V_simulation, eta_k=eta)
             # update the system position
             X[i + 1] = self.system.x
             v[i + 1] = self.system.v
 
-        return X, v, eta1,  delta_eta
+        return X, v, eta1, delta_eta
 
     def MSM(self, X, lag_time):
 
         """
         :param X: simulation path
+        :param n_states: number of markov states
         :return: Transition matrix
 
         """
-
-        bins = np.linspace(min(X) + 0.05, max(X) - 0.05, self.n_states - 1)
+        lag_time = int(lag_time)
+        bins = np.linspace(-1.7, 1.6, self.n_states - 1)
         discretized_path = np.digitize(X, bins)
 
         """
@@ -155,9 +155,9 @@ class underdamped():
 
         count_matrix = 0.5 * (count_matrix + np.transpose(count_matrix))
 
-        transition_matrix = count_matrix / np.sum(count_matrix, axis=1, keepdims=True)
+        transition_matrix = count_matrix / np.sum(count_matrix, axis= 1, keepdims=True)
 
-        return transition_matrix, discretized_path
+        return transition_matrix
 
     def equilibrium_dist(self, transition_matrix):
         A = np.transpose(transition_matrix) - np.eye(self.n_states)
@@ -180,27 +180,27 @@ class underdamped():
         :return: reweighting factors for each path
         """
 
-        len_paths = int((len(X) - self.tau) / self.lagtime)  # length of the paths generated from sliding window method
+        len_paths = int((len(X) - self.tau))  # length of the paths generated from sliding window method
 
         """ calculate the reweighting factor for each observed path """
         M = np.zeros(len_paths)
         for i in range(len_paths):
             """calculate eta and delta_eta for each path"""
-            eta_ = eta[i*self.lagtime : (i + 1) * self.lagtime + self.tau: self.lagtime]
-            delta_eta_ = delta_eta[i*self.lagtime : (i + 1) * self.lagtime + self.tau: self.lagtime]
+            eta_ = eta[i : i + self.tau + 1]
+            delta_eta_ = delta_eta[i : i + self.tau +1]
 
             """calculate the reweighting factor"""
 
             if delta_eta1 is not None:    # switch between schemes with single and two random numbers
-                eta_1 = eta1[i * self.lagtime: (i + 1) * self.lagtime + self.tau: self.lagtime]
-                delta_eta_1 = delta_eta1[i * self.lagtime: (i + 1) * self.lagtime + self.tau: self.lagtime]
+                eta_1 = eta1[i: i + self.tau + 1]
+                delta_eta_1 = delta_eta1[i: i + self.tau + 1]
 
-                M[i] = np.exp((self.V_simulation.potential(X[i*self.lagtime]) - self.V_target.potential(X[i*self.lagtime])) / self.system.T) * (
+                M[i] = np.exp((self.V_simulation.potential(X[i]) - self.V_target.potential(X[i])) / (kb * self.system.T)) * (
                            np.exp(-np.sum(eta_ * delta_eta_)) * np.exp(-0.5 * np.sum(delta_eta_ ** 2))) * (
                            np.exp(-np.sum(eta_1 * delta_eta_1)) * np.exp(-0.5 * np.sum(delta_eta_1 ** 2)))
             else:
-                M[i] = np.exp((self.V_simulation.potential(X[i * self.lagtime]) - self.V_target.potential(
-                    X[i * self.lagtime])) / self.system.T) * (np.exp(-np.sum(eta_ * delta_eta_)) * np.exp(-0.5 * np.sum(delta_eta_ ** 2)))
+                M[i] = np.exp((self.V_simulation.potential(X[i]) - self.V_target.potential(
+                    X[i])) / (kb * self.system.T)) * (np.exp(-np.sum(eta_ * delta_eta_)) * np.exp(-0.5 * np.sum(delta_eta_ ** 2)))
 
         return M
 
@@ -214,20 +214,30 @@ class underdamped():
         :return: reweighted transition matrix
         """
 
-        tol = 0
-        bins = np.linspace(min(X) + tol, max(X) - tol, self.n_states - 1)
-        len_paths = int((len(X) - self.tau) / self.lagtime)
+        # X = X[(X <= 1.6) & (X >= -1.7)]
+        bins = np.linspace(-2, 2, self.n_states + 1)
+        len_paths = int(len(X) - self.tau)
         count_matrix = np.zeros((self.n_states, self.n_states))
-        path_len = int(self.tau/self.lagtime)
+
+        discretized_path = np.digitize(X, bins)
+        discretized_path[discretized_path == 101] = 100
+        discretized_path[discretized_path == 0] = 1
+        discretized_path -= 1
 
         for i in range(len_paths):
-            path = np.digitize(X[i * self.lagtime : (i + 1) * self.lagtime + self.tau : self.lagtime], bins)
+            path = discretized_path[i: i + self.tau + 1]
 
-            for j in range(path_len):
-                count_matrix[path[j], path[j + 1]] += 1 * M[i]
+            '''transitions = path[:self.tau - lag_time], path[lag_time:]  # Two slices for transitions
+
+            # Count the transitions
+            for start, end in zip(*transitions):
+                count_matrix[start, end] += M[i]'''
+
+            count_matrix[path[0], path[-1]] += M[i]
 
         count_matrix = 0.5 * (count_matrix + np.transpose(count_matrix))
 
         transition_matrix = count_matrix / np.sum(count_matrix, axis=1, keepdims=True)
 
         return transition_matrix
+
